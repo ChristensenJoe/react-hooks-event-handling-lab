5f258da7bd5c1184aef22f4f7f39c79b564d596d
Joe Christensen <joechristensenwork@gmail.com>
2021-07-21 15:02:10 -0700

    commit tree 2393574fdc93741364bf210b4b597c415bb8e4a2

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+
+function Keypad() {
+    function keypadOnChange() {
+        console.log("Entering password...");
+    }
+    return (
+        <input onChange={keypadOnChange} type="password" />
+    )
+}
+
+export default Keypad;
